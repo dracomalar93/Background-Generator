a250f9596fb82a30558eef6adbd3c4ddb38614a7
dracomalar93 <120229814+dracomalar93@users.noreply.github.com>
2023-02-01 18:21:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-let css = document.querySelector("h3");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let body = document.getElementById("gradient");
-function setGradient() {
-	body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
+// Wait for the DOM to finish loading before running the JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+  // Function to set the initial background color based on the color inputs
+  const initialColors = () => {
+  const color1Input = document.querySelector("#color1");
+  const color2Input = document.querySelector("#color2");
 
-	css.textContent = body.style.background + ";";
+  if (color1Input && color2Input) {
+    const color1 = color1Input.value;
+    const color2 = color2Input.value;
+    document.querySelector("body").style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+  }
+};
+
+
+  // Call the initialColors function
+  initialColors();
+
+  // Get the random button
+  const randomButton = document.querySelector("#random");
+  // Add a click event listener to the random button
+  randomButton.addEventListener("click", function() {
+    // Generate two random colors
+    const color1 = getRandomColor();
+    const color2 = getRandomColor();
+    // Update the color inputs with the random colors
+    document.querySelector("#color1").value = color1;
+    document.querySelector("#color2").value = color2;
+    // Update the background color
+    document.querySelector("body").style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+  });
+});
+
+// Function to generate a random color string in the format "#RRGGBB"
+function getRandomColor() {
+  // The possible characters for each color component
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  // Generate a random color string by picking random characters from the `letters` string
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
-
-color1.addEventListener("input", setGradient);
-
-color2.addEventListener("input", setGradient);
